c4a1885f3d50e75a8b4e5ccfc06eae34d07b9766
PGKTiT1 <113348887+PGKTiT1@users.noreply.github.com>
2022-09-12 13:44:50 +0300

    new cite

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.vid').click(function() {
+    $('.block').toggleClass('block_long');
+    $('.block_vertical').toggleClass('block_long');
+    $('.block_horizontal').toggleClass('block_long');
+    $('.big_block').toggleClass('block_long');
+    });
+});
